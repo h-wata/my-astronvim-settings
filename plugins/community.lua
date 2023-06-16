9b6e162307c5e2a9bfb21d2207150e2b73edddc7
h-wata <hirofumi01.watanabe@g.softbank.co.jp>
2023-06-16 20:40:00 +0900

    core.lua

--- plugins/community.lua
+++ plugins/community.lua
@@ -5,5 +5,7 @@ return {
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
 
   { import = "astrocommunity.colorscheme.iceberg-vim" },
+  { import = "astrocommunity.colorscheme.dracula-nvim" },
+  { import = "astrocommunity.colorscheme.catppuccin" },
   { import = "astrocommunity.completion.copilot-lua-cmp" },
 }
--- plugins/core.lua
+++ plugins/core.lua
@@ -24,15 +24,45 @@ return {
   -- { "max397574/better-escape.nvim", enabled = false },
   --
   -- You can also easily customize additional setup of plugins that is outside of the plugin's setup call
-  -- {
-  --   "L3MON4D3/LuaSnip",
-  --   config = function(plugin, opts)
-  --     require "plugins.configs.luasnip"(plugin, opts) -- include the default astronvim config that calls the setup call
-  --     -- add more custom luasnip configuration such as filetype extend or custom snippets
-  --     local luasnip = require "luasnip"
-  --     luasnip.filetype_extend("javascript", { "javascriptreact" })
-  --   end,
-  -- },
+  {
+    "L3MON4D3/LuaSnip",
+    config = function(plugin, opts)
+      require "plugins.configs.luasnip" (plugin, opts) -- include the default astronvim config that calls the setup call
+      -- add more custom luasnip configuration such as filetype extend or custom snippets
+      local luasnip = require "luasnip"
+      local snip = luasnip.snippet
+      local text = luasnip.text_node
+      local insert = luasnip.insert_node
+      luasnip.filetype_extend("javascript", { "javascriptreact" })
+      luasnip.add_snippets("all", {
+        snip({
+          trig = "init-env",
+        }, {
+          text({ '#!/usr/bin/env python3', '# -*- encoding: utf-8 -*-', '' }),
+          insert(0),
+        }),
+        snip({
+          trig = "apache",
+        }, {
+          text({
+            '# Copyright (c) 2023 SoftBank Corp.',
+            '#',
+            '# Licensed under the Apache License, Version 2.0 (the "License");',
+            '# you may not use this file except in compliance with the License.',
+            '# You may obtain a copy of the License at',
+            '#',
+            '#     http://www.apache.org/licenses/LICENSE-2.0',
+            '#',
+            '# Unless required by applicable law or agreed to in writing, software',
+            '# distributed under the License is distributed on an "AS IS" BASIS,',
+            '# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.',
+            '# See the License for the specific language governing permissions and',
+            '# limitations under the License.', '' }),
+          insert(0),
+        }),
+      })
+    end,
+  },
   -- {
   --   "windwp/nvim-autopairs",
   --   config = function(plugin, opts)
